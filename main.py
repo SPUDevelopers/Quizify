0fb18d237ff36f7c9c3ddda35a5f67371b58f3fe
Aaron Grider <aaron.grider@gmail.com>
2015-02-28 15:42:35 -0800

    Fixed diff issues

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ SPUD Hackathon
 Aaron Grider, Joe Jazdzewski, Isaac Wang
 """
 
-<<<<<<< HEAD
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
-
-=======
 #!/usr/bin/env python3
 
 import sys
@@ -39,7 +31,7 @@ if __name__ == "__main__":
     root.geometry("800x600")
 
     # Add quit button
-    quit = Button(root, text="Quit", command=root.quit, d)
+    quit = Button(root, text="Quit", command=root.quit)
     quit.pack()
 
     root.withdraw() # Hide main window
@@ -58,4 +50,3 @@ if __name__ == "__main__":
     root.deiconify() # Show main window
 
     root.mainloop()
->>>>>>> origin/master
